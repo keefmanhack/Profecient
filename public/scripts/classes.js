0d097bb13be62f3e12a5e6f53b35720a28165d0d
Keefer Gregoire <keefergregoire@Keefers-MacBook-Pro.local>
2020-04-30 00:57:03 -0400

    complete

--- public/scripts/classes.js
+++
@@ -1,8 +0,0 @@
-$(".dropdown").click(function(e){
-	var classContent = $(this).parent().next();
-	if(classContent.is(":visible")){
-		classContent.hide("fast", "linear");
-	}else{
-		classContent.show("fast", "linear");
-	}
-});
